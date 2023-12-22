7fa7e5ccd39f985e5aa092828f893417fd00ccaf
dbuesa <d.buesa@sapalomera.cat>
2023-12-22 09:10:10 +0100

    p

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -13,7 +13,7 @@ export class FoodService {
       {
         id:1,
         name: 'Assortment of fruit',
-        price: 4,
+        price: 3.99S,
         cookTime: '2-3',
         favorite: true,
         origins: ['Morocco', 'Spain'],
